1b42b2bb3508a5bfd801095b85a24d63d747c501
Faseun <faseunoluwatobilobasimon@gmail.com>
2023-06-02 09:59:12 +0100

    created the usermodel and the dbcontext

---
+++ Entities/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Entities.Models
+{
+    public class UserModel : IdentityUser
+    {
+        public UserModel() : base()
+        {
+            Technologies = new HashSet<TechnologiesModel>();
+            Projects = new HashSet<ProjectsModel>();
+            WorkExperiences = new HashSet<WorkExperienceModel>();
+
+            IsDeleted = false;
+            IsActive = true;
+            CreatedDate = DateTime.Now;
+            UpdatedDate = DateTime.Now;
+
+        }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<TechnologiesModel> Technologies { get; set; }
+        public ICollection <ProjectsModel> Projects { get; set; }
+        public ICollection <WorkExperienceModel> WorkExperiences { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Repository/MyProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class MyProjectDbContext : IdentityDbContext<UserModel>
+    {
+
+        public MyProjectDbContext(DbContextOptions<MyProjectDbContext> options) : base(options)
+        {
+           
+        }
+        public DbSet<ProjectsModel> Projects { get; set; }
+        public DbSet<TechnologiesModel> Technologies { get; set; }
+        public DbSet<WorkExperienceModel> WorkExperiesnces { get; set; }
+        public DbSet<ProjectTechnologiesModel> ProjectTechnologies { get; set; }
+    }
+}

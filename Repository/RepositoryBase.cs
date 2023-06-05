8a0b63a0c6fd30106ba72bedbc225b028713291d
Faseun <faseunoluwatobilobasimon@gmail.com>
2023-06-05 11:52:34 +0100

    created the repositorybase contect and repository

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected MyProjectDbContext Context;
+
+        public RepositoryBase(MyProjectDbContext context)
+        {
+            Context = context;
+        }
+        public void Create(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+         => !trackChanges ? Context.Set<T>()
+                .AsNoTracking() :
+                Context.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expresion, bool trackChanges)
+        =>
+            !trackChanges ? Context.Set<T>().Where(expresion).AsNoTracking() :
+            Context.Set<T>().Where(expresion);
+
+        public void Update(T entity)
+        {
+            Context.Set<T>().Update(entity);
+        }
+    }
+}
